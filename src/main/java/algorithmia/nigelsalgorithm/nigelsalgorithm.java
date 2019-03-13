497f3d5910e03391fc6a7594a423fca2554c804b
ons <adrian.harris@ons.gov.uk>
2019-03-13 16:08:03 +0000

    Algorithmia initial commit

---
+++ src/main/java/algorithmia/nigelsalgorithm/nigelsalgorithm.java
@@ -0,0 +1,13 @@
+package algorithmia.nigelsalgorithm;
+
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
+public class nigelsalgorithm {
+    public String apply(String s) throws Exception {
+        return "Hello " + s;
+    }
+}
